6ef36bd2fa262d2fc9d85befe2d1075998976a59
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2024-01-04 01:13:17 +0100

    First implementation

---
+++ src/main/java/com/unir/forum/publisher/service/PublisherService.java
@@ -0,0 +1,47 @@
+package com.unir.forum.publisher.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PublisherService {
+
+    private final JmsTemplate topicJmsTemplate;
+    private final JmsTemplate queueJmsTemplate;
+    private final ObjectMapper objectMapper;
+
+    /**
+     * Publica un mensaje en un topic.
+     * @param topic - nombre del topic.
+     * @param message - mensaje a enviar.
+     */
+    public void publishToTopic(String topic, String message){
+        topicJmsTemplate.convertAndSend(topic, message);
+    }
+
+    /**
+     * Publica un mensaje en una cola.
+     * @param queue - nombre de la cola.
+     * @param message - mensaje a enviar.
+     */
+    public void publishToQueue(String queue, String message){
+        queueJmsTemplate.convertAndSend(queue, message);
+    }
+
+
+    /**
+     * Escucha los mensajes que llegan a una cola.
+     * @param message - mensaje recibido.
+     */
+    @JmsListener( destination = "${forum.userId}")
+    public void listenDirectMessage(String message) {
+        log.info("Se ha recibido un mensaje privado: " + message);
+    }
+
+}
